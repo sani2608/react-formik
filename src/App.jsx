f6511690bd16731443fc2d71b1f3ed287106bf8a
sanikumar <sanikumarsahani@gmail.com>
2022-06-26 01:14:16 +0530

    course enrolment form

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from "react";
 import "./App.css";
+import EnrolmentForm from "./components/EntolmentForm/EnrolmentForm";
 import FormikContainer from "./components/FormikContainer";
 import LoginForm from "./components/LoginForm/LoginForm";
 import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       {/* <YoutubeForm /> */}
       {/* <FormikContainer /> */}
       {/* <LoginForm /> */}
-      <RegistrationForm />
+      {/* <RegistrationForm /> */}
+      <EnrolmentForm />
     </div>
   );
 }
